d36be69040a32a34f1a57762cd29afef66316403
NightScript <maorninja@outlook.com>
2021-04-14 21:38:50 -0700

    Fix existsSync and remove

--- deno.js
+++ deno.js
@@ -1,4 +1,5 @@
 import puppeteer from "https://deno.land/x/puppeteer@5.5.1/mod.ts";
+import { existsSync } from "https://deno.land/std/fs/mod.ts";
 
 const web = Deno.args.includes("web");
 let jekyll = null;
@@ -36,14 +37,14 @@ for (const dir of rootPagesFolder) {
 		let rootPath = `${dir.substr(1)}/${page.substr(0, page.indexOf("."))}`;
 		await tab.goto((web ? "https://manual.ds-homebrew.com/" : "http://127.0.0.1:4000/") + rootPath, {waitUntil: "networkidle0"});
 
-		if (!Deno.existsSync(`nitrofiles/pages/${rootPath}.gif`) || Deno.statSync(`nitrofiles/pages/${rootPath}.gif`).mtime < Deno.statSync(`pages/${dir}/${page}`).mtime) {
+		if (!existsSync(`nitrofiles/pages/${rootPath}.gif`) || Deno.statSync(`nitrofiles/pages/${rootPath}.gif`).mtime < Deno.statSync(`pages/${dir}/${page}`).mtime) {
 			await tab.screenshot({path: "screenshot.png"});
 			Deno.run({ cmd: ["ffmpeg", "-i", "screenshot.png", "-vf", "palettegen=max_colors=246", "palette.png", "-y"]});
 			const crop = Deno.run({ cmd: ["ffmpeg", "-loop", "1", "-i", "screenshot.png", "-frames:v", "3", "-vf", "negate,cropdetect=0:2:0", "-f", "null", "-"]}).stderr.toString().match(/crop=.*:.*:.*:.*/)[0];
 			Deno.run({ cmd: ["ffmpeg", "-i", "screenshot.png", "-i", "palette.png", "-filter_complex", `${crop},paletteuse`, `nitrofiles/pages/${rootPath}.gif`, "-y"]}).stderr.toString();
 		}
 
-		if (!Deno.existsSync(`nitrofiles/pages/${rootPath}.ini`) || Deno.statSync(`nitrofiles/pages/${rootPath}.ini`).mtime < Deno.statSync(`pages/${dir}/${page}`).mtime) {
+		if (!existsSync(`nitrofiles/pages/${rootPath}.ini`) || Deno.statSync(`nitrofiles/pages/${rootPath}.ini`).mtime < Deno.statSync(`pages/${dir}/${page}`).mtime) {
 			let links = await tab.evaluate(() => {
 				let out = `
 [INFO]
@@ -77,7 +78,7 @@ await browser.close();
 if (jekyll)
 	jekyll.close();
 
-if (Deno.existsSync("screenshot.png"))
-	Deno.rmSync("screenshot.png");
-if (Deno.existsSync("palette.png"))
-	Deno.rmSync("palette.png");
+if (existsSync("screenshot.png"))
+	Deno.removeSync("screenshot.png");
+if (existsSync("palette.png"))
+	Deno.removeSync("palette.png");
